2d6c28a1ae36c8acfc2b9c604c031eac94a611e2
Sean Davis <seandavi@gmail.com>
2016-01-04 22:53:35 +0000

    Think this is working.

---
+++ src/abc.cpp
@@ -0,0 +1,47 @@
+#include <Rcpp.h>
+#include <ncbi-vdb/NGS.hpp>
+#include <ngs-bam/ngs-bam.hpp>
+#include <ngs/ErrorMsg.hpp>
+#include <ngs/ReadCollection.hpp>
+#include <ngs/ReadIterator.hpp>
+#include <ngs/Read.hpp>
+
+
+#include <math.h>
+#include <iostream>
+
+using namespace ngs;
+using namespace std;
+using namespace Rcpp;
+
+// This is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using the Rcpp::sourceCpp 
+// function (or via the Source button on the editor toolbar). Learn
+// more about Rcpp at:
+//
+//   http://www.rcpp.org/
+//   http://adv-r.had.co.nz/Rcpp.html
+//   http://gallery.rcpp.org/
+//s
+
+//
+// [[Rcpp::export]]
+NumericVector readCount(CharacterVector accVec) {
+  ReadCollection run = ncbi::NGS::openReadCollection ( "SRR619505" );
+  return 123;
+}
+
+// [[Rcpp::export]]
+NumericVector timesTwo(NumericVector x) {
+  return x * 2;
+}
+
+
+// You can include R code blocks in C++ files processed with sourceCpp
+// (useful for testing and development). The R code will be automatically 
+// run after the compilation.
+//
+
+/*** R
+timesTwo(42)
+*/
