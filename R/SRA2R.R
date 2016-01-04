fd0b20f1e0c4ae7ded0a2071b705369bb36e9b3e
Sean Davis <seandavi@gmail.com>
2016-01-04 19:42:18 +0000

    Initial package skeleton

---
+++ R/SRA2R.R
@@ -0,0 +1,17 @@
+#' SRA2R: A bridge between the NCBI Sequence Read Archive (SRA) and R
+#'
+#' The SRA2R package provides three categories of important functions:
+#' 
+#' @section Convenience functions:
+#' Install the SRA Toolkit automatically
+#' 
+#' @section Access SRA data:
+#' fastq-dump
+#' sam-dump
+#' sra-stat
+#' sra-pileup
+#'
+#' @docType package
+#' @name SRA2R
+NULL
+#> NULL
