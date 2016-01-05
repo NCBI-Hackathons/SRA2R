f1cd05561c7f5ab92ff04a7908b9ef3af5cf47a4
stineaj <stineaj@ncbi.nlm.nih.gov>
2016-01-05 17:05:22 -0500

    Update test_getReference.R

--- tests/testthat/test_getReference.R
+++ tests/testthat/test_getReference.R
@@ -2,5 +2,5 @@ library(SRA2R)
 context("getReference")
 
 test_that("getReference returns references from a run", {
-  (nrow(getReference('SRR390728')),25)
+  expect_equal(nrow(getReference('SRR390728')),25)
 })
