c5b74d24071cf458387525cebbaa28c8f5c9ee49
Sean Davis <seandavi@gmail.com>
2016-01-06 02:01:09 +0000

    Adding Bioconductor bindings for Biostrings
    Followed the steps up to step c as recommended in
    Biostrings_interface.h. Code with the includes
    now compiles.

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -28,3 +28,90 @@ BEGIN_RCPP
     return __result;
 END_RCPP
 }
+// getPileUp
+DataFrame getPileUp(Rcpp::String acc, Rcpp::String refname, int start, int stop, int MinPileUpDepth);
+RcppExport SEXP SRA2R_getPileUp(SEXP accSEXP, SEXP refnameSEXP, SEXP startSEXP, SEXP stopSEXP, SEXP MinPileUpDepthSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< Rcpp::String >::type acc(accSEXP);
+    Rcpp::traits::input_parameter< Rcpp::String >::type refname(refnameSEXP);
+    Rcpp::traits::input_parameter< int >::type start(startSEXP);
+    Rcpp::traits::input_parameter< int >::type stop(stopSEXP);
+    Rcpp::traits::input_parameter< int >::type MinPileUpDepth(MinPileUpDepthSEXP);
+    __result = Rcpp::wrap(getPileUp(acc, refname, start, stop, MinPileUpDepth));
+    return __result;
+END_RCPP
+}
+// getFastqCount
+long getFastqCount(Rcpp::String acc);
+RcppExport SEXP SRA2R_getFastqCount(SEXP accSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< Rcpp::String >::type acc(accSEXP);
+    __result = Rcpp::wrap(getFastqCount(acc));
+    return __result;
+END_RCPP
+}
+// getFastqReads
+Rcpp::List getFastqReads(Rcpp::String acc);
+RcppExport SEXP SRA2R_getFastqReads(SEXP accSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< Rcpp::String >::type acc(accSEXP);
+    __result = Rcpp::wrap(getFastqReads(acc));
+    return __result;
+END_RCPP
+}
+// getFastqReadsWithQuality
+Rcpp::List getFastqReadsWithQuality(Rcpp::String acc);
+RcppExport SEXP SRA2R_getFastqReadsWithQuality(SEXP accSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< Rcpp::String >::type acc(accSEXP);
+    __result = Rcpp::wrap(getFastqReadsWithQuality(acc));
+    return __result;
+END_RCPP
+}
+// getSRAReadsWithRegion
+Rcpp::List getSRAReadsWithRegion(Rcpp::String acc, Rcpp::String refname, long start, long stop);
+RcppExport SEXP SRA2R_getSRAReadsWithRegion(SEXP accSEXP, SEXP refnameSEXP, SEXP startSEXP, SEXP stopSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< Rcpp::String >::type acc(accSEXP);
+    Rcpp::traits::input_parameter< Rcpp::String >::type refname(refnameSEXP);
+    Rcpp::traits::input_parameter< long >::type start(startSEXP);
+    Rcpp::traits::input_parameter< long >::type stop(stopSEXP);
+    __result = Rcpp::wrap(getSRAReadsWithRegion(acc, refname, start, stop));
+    return __result;
+END_RCPP
+}
+// getBamReadsWithRegion
+Rcpp::List getBamReadsWithRegion(Rcpp::String acc, Rcpp::String refname, long start, long stop);
+RcppExport SEXP SRA2R_getBamReadsWithRegion(SEXP accSEXP, SEXP refnameSEXP, SEXP startSEXP, SEXP stopSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< Rcpp::String >::type acc(accSEXP);
+    Rcpp::traits::input_parameter< Rcpp::String >::type refname(refnameSEXP);
+    Rcpp::traits::input_parameter< long >::type start(startSEXP);
+    Rcpp::traits::input_parameter< long >::type stop(stopSEXP);
+    __result = Rcpp::wrap(getBamReadsWithRegion(acc, refname, start, stop));
+    return __result;
+END_RCPP
+}
+// getReference
+DataFrame getReference(Rcpp::String acc);
+RcppExport SEXP SRA2R_getReference(SEXP accSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< Rcpp::String >::type acc(accSEXP);
+    __result = Rcpp::wrap(getReference(acc));
+    return __result;
+END_RCPP
+}
